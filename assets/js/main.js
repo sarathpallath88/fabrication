beb14779a1624353d0ad98c256b1ff99d3ba19e0
sarath <pallathsarath88@gmail.com>
2023-07-11 12:17:54 +0530

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 /**
-* Template Name: MyResume
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
+* Template Name: MyResume - v4.9.2
 * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
